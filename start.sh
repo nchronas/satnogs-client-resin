b16d3d52f7bf55b28de6c7af7c9c6ab1d669dd90
nchronas <nchronas@gmail.com>
2016-06-13 14:23:27 +0300

    changed to openssh

--- start.sh
+++ start.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-#Set the root password as root if not set as an ENV variable
-export PASSWD=${PASSWD:=root}
-#Set the root password
-echo "root:$PASSWD" | chpasswd
+
+# Make sudo actually work
+HOSTNAME=$(cat /etc/hostname)
+echo "127.0.1.1 $HOSTNAME" >> /etc/hosts
+hostname $HOSTNAME
+
+if [ "$INITSYSTEM" != "on" ]; then
+  /usr/sbin/sshd -p 80 &
+fi
